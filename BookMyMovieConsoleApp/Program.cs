32a13be2b030ebe0477a0370a95fe0cbc01460a2
Shalu07Yadav <shalu.cse.21@nitap.ac.in>
2025-02-20 19:37:04 +0530

    Add project files.

---
+++ BookMyMovieConsoleApp/Program.cs
@@ -0,0 +1,166 @@
+﻿using BookMyMovieDataAccessLayer;
+using BookMyMovieDataAccessLayer.Models;
+
+namespace BookMyMovieConsoleApp
+{
+    internal class Program
+    {
+
+        public static BookMyMovieRepository Repository { get; set; }
+        static Program()
+        {
+            Repository = new BookMyMovieRepository();
+        }
+        static void Main(string[] args)
+        {
+            //TestGetAllUsers();
+            //TestGetBookings();
+            //TestAddMovie();
+            //TestAddTheatre();
+            //TestUpdateUserDetails();
+            //TestCancelBooking();
+           // TestDeleteUser();
+
+        }
+
+        public static void TestGetAllUsers()
+        {
+            List<User> users = Repository.GetAllUsers();
+
+            if (users != null)
+            {
+                for (int i = 0; i < users.Count; i++)
+                {
+                    Console.WriteLine("Id: " + users[i].UserId + " Name: " + users[i].FirstName + " Email: " + users[i].Email);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No users found or an error occurred.");
+            }
+
+        }
+
+        public static void TestGetBookings()
+        {
+            List<Booking> bookings = Repository.GetBookings();
+            if (bookings != null && bookings.Count > 0)
+            {
+                Console.WriteLine("Bookings where seats booked > 3:");
+                foreach (var booking in bookings)
+                {
+                    Console.WriteLine("Booking ID:" + booking.BookingId + " User ID: " + booking.UserId + " Movie Id: " + booking.MovieId + "Theater Id:" + booking.TheaterId + " Seats Booked: " + booking.SeatsBooked);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No bookings found or an error occurred.");
+            }
+        }
+
+        public static void TestAddMovie()
+        {
+            Movie newMovie = new Movie
+            {
+                Title = "Interstellar",
+                Genre = "Sci-Fi",
+                Duration = 169, // Duration in minutes 
+                ReleaseDate = new DateOnly(2014, 11, 7)
+            };
+
+            bool isAdded = Repository.AddMovie(newMovie);
+            if (isAdded)
+            {
+                Console.WriteLine("Movie added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to add the movie.");
+
+            }
+
+        }
+
+        public static void TestAddTheatre()
+        {
+            Theater newTheater = new Theater
+            {
+                Name = "Grand Cinemas",
+                Location = "Los Angeles, CA",
+                TotalSeats = 300
+            };
+
+            bool isAdded = Repository.AddTheatre(newTheater);
+
+            if (isAdded)
+            {
+                Console.WriteLine("Theater added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to add the mtheater.");
+
+            }
+
+        }
+
+
+        public static void TestUpdateUserDetails()
+        {
+            User updatedUser = new User
+            {
+                UserId = 1, // Assuming a user with ID 1 exists
+                Email = "new.email@example.com",
+                ContactNumber = "9998887776"
+            };
+
+            int updateResult = Repository.UpdateUserDetails(updatedUser);
+            if (updateResult == 1)
+            {
+                Console.WriteLine("User details updated successfully.");
+            }
+            else if(updateResult == -1)
+            {
+                Console.WriteLine("User not found.");
+            }
+            else
+            {
+                Console.WriteLine("An error occurred while updating user Details");
+            }
+        }
+
+        public static void TestCancelBooking()
+        {
+            int bookingId = 1;
+            int cancelResult = Repository.CancelBooking(bookingId);
+            if (cancelResult == 1)
+            {
+                Console.WriteLine("Booking cancelled successfully.");
+            }
+            else if (cancelResult == -1)
+            {
+                Console.WriteLine("Booking not found.");
+            }
+            else
+            {
+                Console.WriteLine("An error occurred while canceling the Booking");
+            }
+        }
+
+
+        public static void TestDeleteUser()
+        {
+            int userId = 4;
+            bool deleteResult = Repository.DeleteUser(userId);
+            if (deleteResult)
+            {
+                Console.WriteLine("User deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("User could not be deleted either does not  exist or has associated bookings");
+            }
+        }
+    }
+}
+
---
+++ BookMyMovieDataAccessLayer/BookMyMovieRepository.cs
@@ -0,0 +1,176 @@
+﻿using BookMyMovieDataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyMovieDataAccessLayer
+{
+    public class BookMyMovieRepository
+    {
+        public BookMyMovieDbContext Context { get; set; }
+        public BookMyMovieRepository()
+        {
+            Context = new BookMyMovieDbContext();
+        }
+
+        //read operation
+        public List<User> GetAllUsers()
+        {
+            List<User> Users= new List<User>();
+            try
+            {
+                Users=Context.Users.ToList();
+            }
+            catch (Exception ex)
+            {
+                Users = null;
+            }
+
+            return Users;
+        }
+
+        //get booking
+        public List<Booking> GetBookings()
+        {
+            List<Booking> Bookings = new List<Booking>();
+            try
+            {
+                Bookings=(from b in Context.Bookings
+                          where b.SeatsBooked>=3
+                          orderby b.TheaterId 
+                          select b).ToList();
+               
+            }
+            catch (Exception ex)
+            {
+
+                Bookings = null;
+            }
+            return Bookings;
+        }
+
+        //add movie
+        public bool AddMovie(Movie movie)
+        {
+            bool status = false;
+            try
+            {
+                Context.Movies.Add(movie);
+                Context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+            }
+            return status;
+        }
+
+        //add theater
+        public bool AddTheatre(Theater theater)
+        {
+            bool status = false;
+            try
+            {
+               Context.Theaters.Add(theater);
+                Context.SaveChanges();
+                status = true;
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+            }
+            return status;
+        }
+
+        //update user details
+        public int UpdateUserDetails(User user)
+        {
+
+            int status = 0;
+            try
+            {
+              Context.Users.Add(user);
+                Context.SaveChanges();
+                status = 1;
+            }
+            catch (Exception ex)
+            {
+
+                status = -99;
+            }
+
+            return status;
+        }
+
+        //cancel
+        public int CancelBooking(int bookingId)
+        {
+            int status = 0;
+            Booking booking= Context.Bookings.Find(bookingId);
+            if (booking == null)
+            {
+                return -1;
+            }
+            try
+            {
+                Context.Bookings.Remove(booking);
+                Context.SaveChanges();
+                status = 1;
+            }
+            catch (Exception ex)
+            {
+
+                status = -99;
+            }
+            return status;
+        }
+
+        //7th 
+        public bool DeleteUser(int userId)
+        {
+            User user = Context.Users.Find(userId);
+            var temp = AlreadyBooking(userId);
+
+            bool status= false;
+            try
+            {
+                
+                Context.Users.Remove(user);
+                Context.SaveChanges();
+                status = true;
+                
+            }
+            catch (Exception ex)
+            {
+
+                status = false;
+            }
+            return status;
+        }
+
+        public int AlreadyBooking(int userId)
+        {
+
+            var m = Context.Bookings.Where(u => u.UserId == userId).Select(u => u).First();
+            try
+            {
+                if (m != null)
+                {
+                    return 0;
+                }
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eception message is: {ex.Message}");
+                return -99;
+            }
+        }
+
+    }
+}
